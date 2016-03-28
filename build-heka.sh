f1cda9af982193e0886a22f8301c6f023dcfbb77
Michael Borsuk <mrb@life360.com>
2016-03-28 22:35:56 +0000

    continue strappin boots

--- build-heka.sh
+++ build-heka.sh
@@ -2,14 +2,25 @@
 
 export GO15VENDOREXPERIMENT=1
 
-git submodule init && git submodule update || exit 1
+if ! [ -d "heka" ]; then
+    git clone "git@github.com:mozilla-services/heka.git" || exit $?
+fi
 
-mkdir -p heka/externals/heka-opsee || exit 1
+(cd heka; git checkout v0.10.0) || exit $?
 
-cp -a nsq_input.go vendor heka/externals/heka-opsee || exit 1
+mkdir -p heka/externals/heka-opsee || exit $?
 
-cp heartbeat_filter.lua heka/sandbox/lua/filters/ || exit 1
+cp -a nsq_input.go vendor heka/externals/heka-opsee || exit $?
 
-cp heka-config/plugin_loader.cmake heka/cmake/ || exit 1
+cp heartbeat_filter.lua heka/sandbox/lua/filters/ || exit $?
 
-cd heka; . ./build.sh
+cp heka-config/plugin_loader.cmake heka/cmake/ || exit $?
+
+pushd heka
+source build.sh || exit $?
+pushd docker
+./build_docker.sh || exit $?
+popd
+popd
+
+
