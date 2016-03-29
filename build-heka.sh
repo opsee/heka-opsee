212846397c75f19399353575c20d057648ad5ff0
Michael Borsuk <mrb@life360.com>
2016-03-29 00:15:38 +0000

    moar build script garbage

--- build-heka.sh
+++ build-heka.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export GO15VENDOREXPERIMENT=1
-
 if ! [ -d "heka" ]; then
     git clone "git@github.com:mozilla-services/heka.git" || exit $?
 fi
@@ -15,12 +13,13 @@ cp -a nsq_input.go vendor heka/externals/heka-opsee || exit $?
 cp heartbeat_filter.lua heka/sandbox/lua/filters/ || exit $?
 
 cp heka-config/plugin_loader.cmake heka/cmake/ || exit $?
+cp heka-config/config.toml heka/examples/conf/hekad.toml || exit $?
+cp heka-config/env.sh heka/ || exit $?
+cp heka-config/dockerignore heka/.dockerignore || exit $?
+cp heka-config/Dockerfile heka/ || exit $?
 
-pushd heka
-source build.sh || exit $?
-pushd docker
+pushd heka/docker
 ./build_docker.sh || exit $?
-popd
 popd
 
 
