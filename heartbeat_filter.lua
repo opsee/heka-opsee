7663871cc44ac87ff371a3c72f87340deb561aa8
Michael Borsuk <mrb@life360.com>
2016-04-06 21:20:32 +0000

    wip

--- heartbeat_filter.lua
+++ heartbeat_filter.lua
@@ -1,60 +1,75 @@
 require "string"
-require "circular_buffer"
-
--- total = 0 -- globals preserved btwn restarts
--- local rows              = read_config("rows") or 1440
+require "cjson"
 
 timestamp_field   = "timestamp"
 process_field = "process_name"
-metrics_field = "metrics"
 cust_id_field = "customer_id"
 bast_id_field = "bastion_id"
+metrics_field = "metrics"
 metrics_type_map_field = "metricTypes"
 
-max_num_metrics = 20
-
-values_buf = circular_buffer.new(1440, max_num_metrics, 15)
+metric_names = {
+    ["runtime.MemStats.TotalAlloc"] = 1,
+    ["runtime.MemStats.Alloc"] = 2,
+    ["runtime.MemStats.HeapAlloc"] = 3,
+    ["runtime.MemStats.HeapInuse"] = 4,
+    ["runtime.MemStats.Mallocs"] = 5,
+    ["runtime.MemStats.NumGC"] = 6,
+    ["runtime.MemStats.StackSys"] = 7,
+    ["runtime.MemStats.Sys"] = 8,
+    ["runtime.MemStats.NumThread"] = 9,
+    ["runtime.MemStats.NumGoroutine"] = 10,
+    ["runtime.MemStats.PauseTotalNs"] = 11,
+}
 
 function process_message ()
-	local ts = read_message("Fields["..timestamp_field.."]")
+	local msg = read_message("Payload")
+	if msg == nil then
+		return -1, "empty msg received"
+	end
 
+    local ok, json = pcall(cjson.decode, msg)
+    if not ok then return -1, string.format("Failed to decode JSON: %s", msg) end
+
+	local ts = json[timestamp_field]
     if type(ts) ~= "number" then
-		return -1, string.format("error: bad timestamp received %s", ts)
+		return -1, string.format("bad timestamp received %s", ts)
 	end
 
-	local proc_name = read_message("Fields["..process_field.."]")
+	local proc_name = json[process_field]
 	if proc_name == nil then
-		return -1, string.format("error: empty process name")
+		return -1, string.format("empty process name")
 	end
 
-	local cust_id = read_message("Fields["..cust_id_field.."]")
+	local cust_id = json[cust_id_field]
 	if cust_id == nil then
-		return -1, string.format("error: empty cust id")
+		return -1, string.format("empty cust id")
 	end
 
-	local bast_id = read_message("Fields["..bast_id_field.."]")
+	local bast_id = json[bast_id_field]
 	if bast_id == nil then
-		return -1, string.format("error: empty bast id for %s", cust_id)
+		return -1, string.format("empty bast id for %s", cust_id)
 	end
 
-	local metrics = read_message("Fields["..metrics_field.."]")
-	if metrics == nil then
-		return -1, string.format("error: empty metrics for %s", cust_id)
+	local metrics = json[metrics_field]
+	if metrics == nil or type(metrics) ~= "table" then
+		return -1, string.format("invalid metrics field for %s", cust_id)
 	end
+
 
 	local metrics_types = metrics[metrics_type_map_field]
 	if type(metrics_types) ~= "table" then
-		return -1, string.format("error: empty metrics types for %s", cust_id)
+		return -1, string.format("empty metrics types for %s", cust_id)
 	end
 
 	local errors = 0
-    local metric_col = 0
+    payload = {}
 	for k,v in pairs(metrics) do
         local continue_loop = false
 
 		if k == metrics_type_map_field then
             continue_loop = true
-		elseif type(v) ~= "number" then
+		elseif type(v) ~= "table" then
 			errors = errors+1
             continue_loop = true
 		elseif metrics_types[k] == nil then
@@ -63,26 +78,36 @@ function process_message ()
 		elseif metrics_types[k] ~= "gauge" and metrics_types[k] ~= "gaugeFloat64" then
             -- only support gauge types currently in librato encoder
             continue_loop = true
+        elseif metric_names[k] == nil then
+            --- skip metrics not explicitly listed
+            continue_loop = true 
         end
 
         if not continue_loop then
-            values_buf:set_header(metric_col, cust_id.."_"..proc_name.."_"..k)
-            values_buf:add(ts, metric_col, v)
-
-            if metric_col == max_num_metrics then
-                break
+            val = v["value"]
+            if type(val) ~= "number" then
+                errors = errors +1
+                continue_loop = true
             end
-            metric_col = metric_col+1
+
+            if not continue_loop then
+                payload[k] = val
+            end
         end
 	end
 
 	if errors > 0 then
-		return -1, string.format("error: %d errors processing metrics for %s", errors, cust_id)
+		return -1, string.format("%d errors processing metrics for %s", errors, cust_id)
 	end
 
-    return 0
-end
+    local msg = {
+        Timestamp = ts,
+        Type = 'heartbeat',
+        Payload = cjson.encode(payload),
+        Fields = { customer = cust_id, bastion = bast_id, process = proc_name},
+    }
 
-function timer_event(ns)
-	inject_payload("cbuf", "heartbeat_metrics", values_buf)
+    inject_message(msg)
+
+    return 0
 end
