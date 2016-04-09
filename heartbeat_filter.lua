ee6a625ffa2439cf0215e2d5dc04d110589f7f3e
Michael Borsuk <mrb@life360.com>
2016-04-09 01:01:15 +0000

    add all metrics, output buffer and min flush time

--- heartbeat_filter.lua
+++ heartbeat_filter.lua
@@ -1,5 +1,6 @@
 require "string"
 require "cjson"
+require "table"
 
 timestamp_field   = "timestamp"
 process_field = "process_name"
@@ -9,18 +10,39 @@ metrics_field = "metrics"
 metrics_type_map_field = "metricTypes"
 
 metric_names = {
-    ["runtime.MemStats.TotalAlloc"] = 1,
-    ["runtime.MemStats.Alloc"] = 2,
-    ["runtime.MemStats.HeapAlloc"] = 3,
-    ["runtime.MemStats.HeapInuse"] = 4,
-    ["runtime.MemStats.Mallocs"] = 5,
-    ["runtime.MemStats.NumGC"] = 6,
-    ["runtime.MemStats.StackSys"] = 7,
-    ["runtime.MemStats.Sys"] = 8,
-    ["runtime.MemStats.NumThread"] = 9,
-    ["runtime.MemStats.NumGoroutine"] = 10,
-    ["runtime.MemStats.PauseTotalNs"] = 11,
+    ["runtime.MemStats.TotalAlloc"]		= 1,
+    ["runtime.MemStats.Alloc"]			= 2,
+    ["runtime.MemStats.HeapAlloc"]		= 3,
+    ["runtime.MemStats.HeapInuse"]		= 4,
+	["runtime.MemStats.Mallocs"]		= 5,
+    ["runtime.MemStats.NumGC"]			= 6,
+    ["runtime.MemStats.StackSys"]		= 7,
+    ["runtime.MemStats.Sys"] 			= 8,
+    ["runtime.MemStats.NumThread"] 		= 9,
+    ["runtime.MemStats.NumGoroutine"] 	= 10,
+    ["runtime.MemStats.PauseTotalNs"] 	= 11,
+    ["runtime.MemStats.BuckHashSys"] 	= 12,
+    ["runtime.MemStats.DebugGC"] 		= 13,
+    ["runtime.MemStats.EnableGC"] 		= 14,
+    ["runtime.MemStats.Frees"] 			= 15,
+    ["runtime.MemStats.GCCPUFraction"]  = 16,
+    ["runtime.MemStats.HeapIdle"] 		= 17,
+    ["runtime.MemStats.HeapObjects"] 	= 18,
+    ["runtime.MemStats.HeapReleased"] 	= 19,
+    ["runtime.MemStats.HeapSys"] 		= 20,
+    ["runtime.MemStats.LastGC"] 		= 21,
+    ["runtime.MemStats.Lookups"] 		= 22,
+    ["runtime.MemStats.MCacheInuse"] 	= 23,
+    ["runtime.MemStats.MCacheSys"] 		= 24,
+    ["runtime.MemStats.MSpanSys"] 		= 25,
+    ["runtime.MemStats.NextGC"] 		= 26,
+    ["runtime.MemStats.StackInuse"] 	= 27,
+    ["runtime.MemStats.Sys"] 			= 28,
+    ["runtime.MemStats.NumCgoCall"] 	= 29,
 }
+
+local metric_sets_buffer = {}
+local max_buffer_len = 25
 
 function process_message ()
 	local msg = read_message("Payload")
@@ -56,7 +78,6 @@ function process_message ()
 		return -1, string.format("invalid metrics field for %s", cust_id)
 	end
 
-
 	local metrics_types = metrics[metrics_type_map_field]
 	if type(metrics_types) ~= "table" then
 		return -1, string.format("empty metrics types for %s", cust_id)
@@ -80,7 +101,7 @@ function process_message ()
             continue_loop = true
         elseif metric_names[k] == nil then
             --- skip metrics not explicitly listed
-            continue_loop = true 
+            continue_loop = true
         end
 
         if not continue_loop then
@@ -100,14 +121,42 @@ function process_message ()
 		return -1, string.format("%d errors processing metrics for %s", errors, cust_id)
 	end
 
-    local msg = {
+    local metric_set = {
         Timestamp = ts,
-        Type = 'heartbeat',
-        Payload = cjson.encode(payload),
-        Fields = { customer = cust_id, bastion = bast_id, process = proc_name},
+        Customer = cust_id,
+        Bastion = bast_id,
+        Process = proc_name,
+        Metrics = payload
     }
 
-    inject_message(msg)
+    table.insert(metric_sets_buffer, metric_set)
+
+    if(table.getn(metric_sets_buffer) > max_buffer_len) then
+		send_buffer({{name = "debug_msg_cnt", value=table.getn(metric_sets_buffer)},})
+	end
 
     return 0
+end
+
+function send_buffer(addl_fields)
+	local msg = {
+		Type 	= 'heartbeat',
+		Payload = cjson.encode(metric_sets_buffer),
+		Fields 	= { }
+	}
+
+	if addl_fields ~= nil then
+		for i, field in ipairs(addl_fields) do
+			table.insert(msg.Fields, field)
+		end
+	end
+
+	inject_message(msg)
+	metric_sets_buffer = {}
+end
+
+function timer_event(ns)
+	if(table.getn(metric_sets_buffer) > 0) then
+        send_buffer()
+    end
 end
